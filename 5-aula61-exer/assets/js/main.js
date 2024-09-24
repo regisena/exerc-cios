19042838440d742490d129d6c07622475b8ae118
Reginaldo <regsena@gmail.com>
2024-09-23 21:37:43 -0400

    Commit do projeto 5

---
+++ 5-aula61-exer/assets/js/main.js
@@ -0,0 +1,8 @@
+/**
+ * Escreva uma função chamada ePaisagem que recebe dois argumentos, largura e altura de uma imagem (Number).
+ * Retorne true se a imagem estiver no modo paisagem.
+ */
+
+ const ePaisagem = (width, heigth) =>  width >= heigth;
+
+console.log(ePaisagem(1920, 1080));
